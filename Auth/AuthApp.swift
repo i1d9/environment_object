6e3df1d1e5feb1e6def4604fa033311e5cfa48c1
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2022-11-17 12:58:37 +0300

    Initial Commit

---
+++ Auth/AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApp.swift
+//  Auth
+//
+//  Created by Ian Nalyanya on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
